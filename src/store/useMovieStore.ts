dfe4483c0a96ff591ceb6a1639ae238ee4f38a2c
laska-adief <laskaadief.la@gmail.com>
2024-05-30 20:02:57 +0700

    [Movielog] create Movie store

---
+++ src/store/useMovieStore.ts
@@ -0,0 +1,22 @@
+import { create } from "zustand";
+import { MovieProps } from "../types/movie";
+
+interface MovieStoreProps {
+  page: number;
+  movies: MovieProps[];
+  total_pages: number;
+  setPage: (value: number) => void;
+  setMovies: (values: MovieProps[]) => void;
+  setTotalPages: (values: number) => void;
+}
+
+const useMovieStore = create<MovieStoreProps>((set) => ({
+  page: 0,
+  movies: [],
+  total_pages: 0,
+  setPage: (page: number) => set({ page }),
+  setMovies: (movies: MovieProps[]) => set({ movies }),
+  setTotalPages: (total_pages: number) => set({ total_pages }),
+}));
+
+export default useMovieStore;
