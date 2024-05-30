1f9356aa264bb2d7cac10cf5b94dda8726434d98
laska-adief <laskaadief.la@gmail.com>
2024-05-30 18:35:11 +0700

    [Movielog] add router and QueryClient

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,49 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { RouterProvider, createBrowserRouter, redirect } from "react-router-dom";
+import Movie from "./pages/Movie.tsx";
+import WatchList from "./pages/WatchList.tsx";
+import App from "./App.tsx";
+import { QueryClient, QueryClientProvider } from "react-query";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const router = createBrowserRouter([
+  {
+    path: "",
+    element: <App />,
+    children: [
+      {
+        index: true,
+        loader: () => redirect("/movies"),
+      },
+      {
+        path: "movies",
+        element: <Movie />,
+      },
+      {
+        path: "watchlist",
+        element: <WatchList />,
+      },
+      {
+        path: "*",
+        loader: () => redirect("/movies"),
+      },
+    ],
+  },
+]);
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+    },
+  },
+});
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
+  </React.StrictMode>
+);
