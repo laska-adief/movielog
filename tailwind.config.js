81fd3d5c086d0710a859ec25b31b6ed1638971db
laska-adief <laskaadief.la@gmail.com>
2024-05-30 20:01:01 +0700

    [Movielog] styling app

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      height: {
+        "fill-available": "-webkit-fill-available",
+      },
+    },
   },
   plugins: [],
 };
