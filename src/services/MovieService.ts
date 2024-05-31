79aef71780a5759c39202a7ed8bcab66894503c1
laska-adief <laskaadief.la@gmail.com>
2024-05-31 12:31:19 +0700

    [Movielog] create query getDetailMovie in MovieService

--- src/services/MovieService.ts
+++ src/services/MovieService.ts
@@ -5,9 +5,17 @@ const TOKEN =
   "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2ZDVmNzE5MzU5ZTMyZWIxNGU5NjE5ZWI1ZDk0M2IzNSIsInN1YiI6IjYyM2I5YzYwYTMxNDQwMDA4OWQzMjZiZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.MoFDjZxR2QwrwbvuOz7YLOaLhF338QWZ3CE7nhl969A";
 
 export const getMovies = async (query: string) => {
-  console.log("query", query);
-  console.log(`${BASEURL}/movie/popular${query ? "?" + query : ""}`);
   const { data } = await axios.get(`${BASEURL}/movie/popular${query ? "?" + query : ""}'`, {
+    headers: {
+      accept: "application/json",
+      Authorization: TOKEN,
+    },
+  });
+  return data;
+};
+
+export const getDetailMovie = async (id: number) => {
+  const { data } = await axios.get(`${BASEURL}/movie/${id}`, {
     headers: {
       accept: "application/json",
       Authorization: TOKEN,
